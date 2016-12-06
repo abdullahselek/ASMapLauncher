e40b851b75aa47171d6a87c401b3443b6263404c
abdullahselek <abdullahselek@gmail.com>
2016-12-06 20:29:19 +0100

    - support carthage

---
+++ ASMapLauncherFramework/ASMapLauncherFramework.h
@@ -0,0 +1,19 @@
+//
+//  ASMapLauncherFramework.h
+//  ASMapLauncherFramework
+//
+//  Created by Abdullah Selek on 06/12/2016.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASMapLauncherFramework.
+FOUNDATION_EXPORT double ASMapLauncherFrameworkVersionNumber;
+
+//! Project version string for ASMapLauncherFramework.
+FOUNDATION_EXPORT const unsigned char ASMapLauncherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASMapLauncherFramework/PublicHeader.h>
+
+
